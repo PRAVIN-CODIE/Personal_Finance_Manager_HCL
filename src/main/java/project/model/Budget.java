934fa040261fe75ea0fd5221f82a976ed13911c8
Pravin M <157938749+PRAVIN-CODIE@users.noreply.github.com>
2026-06-24 20:25:36 +0530

    Update Budget.java

--- src/main/java/project/model/Budget.java
+++ src/main/java/project/model/Budget.java
@@ -3,7 +3,10 @@ package com.finance.model;
 import jakarta.persistence.*;
 import lombok.*;
  
-@Entity @Data @NoArgsConstructor @AllArgsConstructor
+@Entity 
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
 public class Budget {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
