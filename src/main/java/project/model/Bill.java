f72d8cc2a61795395fdd632657ee4009107a3c86
Pravin M <157938749+PRAVIN-CODIE@users.noreply.github.com>
2026-06-24 20:28:49 +0530

    Create Bill.java

---
+++ src/main/java/project/model/Bill.java
@@ -0,0 +1,18 @@
+package com.finance.model;
+ 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+ 
+@Entity 
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor
+public class SavingsGoal {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String goalName;
+    private Double targetAmount;
+    private Double savedAmount;
+    private LocalDate targetDate;
+}
